292c03dda7ffff21f281a69eb6b30ccc9a945894
aaron taylor <aarontaylor@gmail.com>
2019-01-25 12:17:43 -0800

    change javascript code to use foreach loop and work when user enters multiple numbers

--- js/script.js
+++ js/script.js
@@ -3,21 +3,26 @@ $(document).ready(function(){
     event.preventDefault();
     var name = $("#name").val();
     var num = $("#num").val();
+    var numbers = num.split(",");
 
-    if ((num != 0) && (num % 3 === 0)) {
-      $("p").append("I'm sorry " + name + " I'm afraid I can't do that! <br>");
-    }
-    else if ((num.indexOf("0") > -1) && (num.indexOf("1") == -1)) {
-      num = "Beep!";
-      $("p").append(num + "<br>");
-    }
-    else if (num.indexOf("1") > -1) {
-      num = "Boop!";
-      $("p").append(num + "<br>");
-    }
-    else {
-      $("p").append(num + "<br>");
-    }
+
+    numbers.forEach(function(number, i) {
+      if ((number != 0) && (number % 3 === 0)) {
+        numbers[i] = " I'm sorry " + name + " I'm afraid I can't do that!";
+      }
+      else if ((number.indexOf("0", i) > -1) && (number.indexOf("1", i) == -1)) {
+        numbers[i] = " Beep!";
+      }
+      else if (number.indexOf("1") > -1) {
+        numbers[i] = " Boop!"
+      }
+      else {
+        number = numbers[1]
+      }
+    });
+
+    $("p").text(numbers);
+
   });
 
 });
