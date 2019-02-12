a20f963415ec700e1964a017efda942438ca3426
aaron taylor <aarontaylor@gmail.com>
2019-02-11 17:50:43 -0800

    separate business logic, fix functionality to match project requirement, use regular for loop

--- js/script.js
+++ js/script.js
@@ -1,28 +1,37 @@
-$(document).ready(function(){
-  $("form#beep").submit(function(event){
+var name = null;
+var num = null;
+var numbers = [];
+
+function beep(number) {
+  for (i = 0; i <= number; i++) {
+
+    var t = i.toString();
+
+    if ((t != 0) && (t % 3 === 0)) {
+      numbers.push(" I'm sorry " + name + " I'm afraid I can't do that!");
+    }
+    else if ((t.indexOf("0", i) > -1) && (t.indexOf("1", i) == -1)) {
+      numbers.push(" Beep!");
+    }
+    else if (t.indexOf("1") > -1) {
+      numbers.push(" Boop!");
+    }
+    else {
+      numbers.push(" " + t);
+    }
+  };
+};
+
+$(document).ready(function() {
+
+  $("form#beep").submit(function(event) {
     event.preventDefault();
-    var name = $("#name").val();
-    var num = $("#num").val();
-    var numbers = num.split(",");
-
-
-    numbers.forEach(function(number, i) {
-      if ((number != 0) && (number % 3 === 0)) {
-        numbers[i] = " I'm sorry " + name + " I'm afraid I can't do that!";
-      }
-      else if ((number.indexOf("0", i) > -1) && (number.indexOf("1", i) == -1)) {
-        numbers[i] = " Beep!";
-      }
-      else if (number.indexOf("1") > -1) {
-        numbers[i] = " Boop!"
-      }
-      else {
-        numbers[i] = " " + number
-      }
-    });
-
+    numbers = [];
+    name = $("#name").val();
+    num = $("#num").val();
+    
+    beep(num);
     $("p").text(numbers);
 
   });
-
 });
